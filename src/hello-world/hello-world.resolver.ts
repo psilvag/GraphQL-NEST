b5043178b4febd5409bfef8d112b22008dca1be2
Pablo Silva <pmarcelosilvag@gmail.com>
2024-03-04 23:51:07 -0400

    Primeros resolvers con GraphQL

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,10 +1,35 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
 
-   @Query(()=>String)// le decimos a graphql que esa funcion retornara un string
+   @Query(()=>String,{description:'Hola mundo es lo que retorna', name:'hello'})// le decimos a graphql que esa funcion retornara un string
    helloWorld():string{
     return 'hola universo'
    }
+
+   @Query(()=>Float,{ name:'randomNumber'})
+   getRandomNumber():number{
+      return Math.random()*100
+   }
+
+   
+   @Query(()=>Int,
+   {
+      name:'randomZero',
+      description:'from zero to argument:to,default(7)'})
+   //@Args('to') ese nombre 'to' dentro de args es lo que usara como referencia GraphQL
+   getRandomNumberFromZeroTo(
+      @Args(
+         'to',
+         {  type:()=>Int, 
+            nullable:true
+         }) 
+         to:number=6):number {
+      return Math.floor(Math.random()*to)
+   }
+   
+   
+
+
 }
